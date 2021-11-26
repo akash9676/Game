818d7f8c37a6334c5e5bc3a06a1d5c872d0968e3
akash9676 <akashnaidu545@gmail.com>
2021-11-27 00:23:00 +0530

    created taste emoji using canvas (not that perfect tho)

---
+++ scripts.js
@@ -0,0 +1,37 @@
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext('2d');
+
+// context.beginPath();
+// context.moveTo(50, 50);
+// context.lineTo(150, 50);
+// context.lineTo(100, 150);
+// context.closePath();
+// context.fillStyle='green';
+
+// context.fill();
+
+// context.beginPath();
+
+// const centerx = canvas.width/2;
+// const centery = canvas.height/2;
+
+// context.arc(centerx, centery, 150, 0, Math.PI * 2 ,true);
+// context.moveTo(centerx+90,centery);
+// context.fillStyle='yellow';
+// context.fill();
+// context.arc(centerx, centery, 90 , 0 , Math.PI,false);
+// context.moveTo(centerx-20, centery-70);
+// context.arc(centerx-50, centery-70, 30, 0, Math.PI * 1, true);
+// context.moveTo(centerx+80, centery-70);
+// context.arc(centerx+50, centery-70, 30, 0, Math.PI * 1, true);
+// context.moveTo(centerx+43, centery+80);
+// context.arc(centerx+60, centery+69, 20, 15, 150, true);
+// context.stroke();
+
+
+
+
+
+
+
+
