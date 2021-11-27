85e2ae848b25d4ce15f6b1191178ecefa0b4a689
akash9676 <akashnaidu545@gmail.com>
2021-11-27 20:48:59 +0530

    comments

--- scripts.js
+++ scripts.js
@@ -15,18 +15,72 @@ const context = canvas.getContext('2d');
 // const centerx = canvas.width/2;
 // const centery = canvas.height/2;
 
-// context.arc(centerx, centery, 150, 0, Math.PI * 2 ,true);
-// context.moveTo(centerx+90,centery);
-// context.fillStyle='yellow';
-// context.fill();
-// context.arc(centerx, centery, 90 , 0 , Math.PI,false);
-// context.moveTo(centerx-20, centery-70);
-// context.arc(centerx-50, centery-70, 30, 0, Math.PI * 1, true);
-// context.moveTo(centerx+80, centery-70);
-// context.arc(centerx+50, centery-70, 30, 0, Math.PI * 1, true);
-// context.moveTo(centerx+43, centery+80);
-// context.arc(centerx+60, centery+69, 20, 15, 150, true);
-// context.stroke();
+// // context.arc(centerx, centery, 150, 0, Math.PI * 2 ,true); 
+// // context.moveTo(centerx+90,centery);
+// // context.fillStyle='yellow';
+// // context.fill();
+// // context.arc(centerx, centery, 90 , 0 , Math.PI,false);
+// // context.moveTo(centerx-20, centery-70);
+// // context.arc(centerx-50, centery-70, 30, 0, Math.PI * 1, true);
+// // context.moveTo(centerx+80, centery-70);
+// // context.arc(centerx+50, centery-70, 30, 0, Math.PI * 1, true);
+// // context.moveTo(centerx+43, centery+80);
+// // context.arc(centerx+60, centery+69, 20, 15, 150, true);
+// // context.stroke();
+// // const circle = {
+// //     x: 200,
+// //     y: 200,
+// //     size: 30,
+// //     dx : 5,
+// //     dy : 5,
+// // }
+
+
+// // context.arc(centerx, centery, 30, 0, Math.PI * 2,true);
+// // context.fillStyle = "green";
+// // context.fill();
+// // context.stroke();
+
+// // context.moveTo(centerx+120, centery);
+
+
+// // context.arc(centerx+90, centery, 30, 0, Math.PI * 2,true);
+// // context.fillStyle = "green";
+// // context.fill();
+// // context.stroke();
+// // function drawCircle(){
+// // context.arc(circle.x, circle.y, 30, 0, Math.PI * 2, true);
+// // context.fillStyle = "green";
+// // context.fill();
+// // context.stroke();
+// // }
+
+// // drawCircle();
+
+// function draw(){
+//     for(var i=0;i<3;i++){
+//         for(var j=0;j<3;j++) {
+            
+//             if((i+j)%2 === 0){
+//                 context.fillStyle='yellow';
+                
+//             }
+//             else {
+//                 context.fillStyle='black';
+                
+//             }
+//             context.save();
+//             context.translate(10 + j * 50, 10 + i * 50);
+//             context.fillRect(0,0,25,25);
+//             context.restore();
+            
+//         }
+//     }
+// }
+// draw();
+
+
+
 
 
 
